54dee6b7017c114bb071bd85d617e57047113e58
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-04-08 13:22:29 +0530

    basic setup done for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+])
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
